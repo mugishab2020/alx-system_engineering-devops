0d2f53a9dbb3bcb87302c919efe0bae0a136f808
mugishab2020 <mugishab2020@gmail.com>
2023-11-08 21:33:55 -0800

    It is not working

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,21 @@
-# finding the issue and sloving it
+# 0-strace_is_your_friend.pp
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Make sure Apache service is running
+service { 'apache2':
+  ensure => 'running',
 }
+
+# Use strace to find the issue and fix it
+exec { 'fix-apache':
+  command     => '/usr/bin/strace -o /tmp/strace_output.txt -f -p $(pidof apache2) & sleep 2; kill $!',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
+
+# Fix the identified issue
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure => file,
+  content => template('my_module/apache_config.erb'), # Use your template
+  notify  => Service['apache2'],
+}
+
