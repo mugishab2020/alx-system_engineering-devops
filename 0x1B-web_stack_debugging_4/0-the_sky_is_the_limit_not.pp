8fa6d878199b95fe73030f04e27853df4044e158
mugishab2020 <mugishab2020@gmail.com>
2023-11-15 12:26:17 -0800

    Now we are solving

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Sky is the limit, let's bring that limit highe
+
+exec { 'fix--for-nginx':
+  command => ['/bin/sed', '-i', 's/15/4096/', '/etc/default/nginx'],
+  path    => ['/usr/local/bin/', '/bin'],
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+  require => Exec['fix--for-nginx'], 
+}
+
