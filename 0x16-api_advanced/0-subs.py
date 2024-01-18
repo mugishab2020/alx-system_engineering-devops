5dc68ae12839f3eee986a923964f50a5d2c54ebe
mugishab2020 <mugishab2020@gmail.com>
2024-01-18 06:31:01 +0000

    trying

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,17 @@ import requests
 def number_of_subscribers(subreddit):
     """query a subreddit and retrive no of subscribers"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    head = {'User-Agent': 'user Agent 1.0'}
-    response = requests.get(url, headers=head, allow_redirects=False)
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        data = response.json().get('data', {})
-        count = data.get('subscribers', 0)
-        return count
+        try:
+
+            data = response.json()
+            subscribers = len(data)
+            return (subscribers)
+        except (KeyError, ValueError):
+            return (0)
+
     else:
-        return 0
+        return (0)
