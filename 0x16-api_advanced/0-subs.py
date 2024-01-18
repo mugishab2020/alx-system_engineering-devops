42894fabc6553d7e61eb28fe652a05f8967bff77
mugishab2020 <mugishab2020@gmail.com>
2024-01-18 06:35:00 +0000

    trying

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        try:
-
-            data = response.json()
-            subscribers = len(data)
-            return (subscribers)
-        except (KeyError, ValueError):
-            return (0)
-
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        return (0)
+        return 0  
