75fdf08fadaac3831604c6889b5a255583f3ac2a
mugishab2020 <mugishab2020@gmail.com>
2024-02-14 07:25:42 +0000

    Now we got this

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        try:
-
-            data = response.json()
-            return data['data']['subscribers']
-        except (KeyError, ValueError):
-            return (0)
+        data = response.json()
+        return data['data']['subscribers']
     else:
         return (0)
