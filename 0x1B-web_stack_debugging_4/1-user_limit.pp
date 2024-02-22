cd7ee7c27a534fe04de56364b005768fc46af4b4
mugishab2020 <mugishab2020@gmail.com>
2024-02-22 17:07:21 +0000

    we got this

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 exec { 'changing soft limit':
-  command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 8192/' /etc/security/limits.conf",
+  command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 5000/' /etc/security/limits.conf",
   provider => 'shell',
 }
 
 exec { 'changing hard limit':
-  command  => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 8192/' /etc/security/limits.conf",
+  command  => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 5000/' /etc/security/limits.conf",
   provider => 'shell',
 }
