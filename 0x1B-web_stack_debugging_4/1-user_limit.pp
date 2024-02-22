06221c195008c281b5d5ebe06d5713ad68579891
mugishab2020 <mugishab2020@gmail.com>
2024-02-22 18:10:31 +0000

    Now we got this

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+#Change the OS configuration so that it is possible to login with the holberton
+#user and open a file without any error message
 exec { 'changing soft limit':
   command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 5000/' /etc/security/limits.conf",
   provider => 'shell',
